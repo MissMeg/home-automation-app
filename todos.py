b2da8814c259cf7d2ba2e945fafb84a45dfb2212
Megan Amendola <megmrob@gmail.com>
2020-06-12 09:11:42 -0500

    todo items

--- todos.py
+++ todos.py
@@ -1,1 +1,7 @@
 # All todo logic
+# todo: create database model
+# todo: create database
+# todo: setup ability to add items to db
+# todo: setup ability to read (get all) items from db
+# todo: setup ability to update items in db
+# todo: setup ability to delete items in db
