1e975a1871eef9496d992c73a1a9b8be3060a5a7
Megan Amendola <megmrob@gmail.com>
2020-06-22 16:58:30 -0500

    added db models

--- grocery.py
+++ grocery.py
@@ -1,7 +1,34 @@
 # All grocery list logic
-# todo: create database model
-# todo: create database
-# todo: setup ability to add items to db
+from peewee import *
+
+
 # todo: setup ability to read (get all) items from db
 # todo: setup ability to update items in db
 # todo: setup ability to delete items in db
+
+DATABASE = SqliteDatabase('glist.db')
+
+class Glist(Model):
+    title = CharField()
+    description = TextField()
+
+    class Meta:
+        database = DATABASE
+
+    @classmethod
+    def create_item(cls, title, description):
+        cls.create(
+            title=title,
+            description=description
+        )
+
+
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([Glist], safe=True)
+    DATABASE.close()
+
+
+if __name__ == '__main__':
+    initialize()
+    Glist.create_item('Peanut butter', "Large one for Jethro's treats.")
--- todos.py
+++ todos.py
@@ -1,7 +1,34 @@
 # All todo logic
-# todo: create database model
-# todo: create database
-# todo: setup ability to add items to db
+from peewee import *
+
+
 # todo: setup ability to read (get all) items from db
 # todo: setup ability to update items in db
 # todo: setup ability to delete items in db
+
+DATABASE = SqliteDatabase('todos.db')
+
+class Todos(Model):
+    title = CharField()
+    location = CharField()
+
+    class Meta:
+        database = DATABASE
+
+    @classmethod
+    def create_todo(cls, title, location):
+        cls.create(
+            title=title,
+            location=location
+        )
+
+
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([Todos], safe=True)
+    DATABASE.close()
+
+
+if __name__ == '__main__':
+    initialize()
+    Todos.create_todo('Walk the dog', 'O')
