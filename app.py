78d97a2a9c7200ed9237c9bb6330e5dbe000fe0b
Megan Amendola <megmrob@gmail.com>
2020-07-08 15:02:21 -0500

    initial html layout
    used bootstrap to layout the initial wireframe of the page

--- app.py
+++ app.py
@@ -3,3 +3,14 @@ from gcalendar import Gcalendar
 from grocery import Glist
 from todos import Todos
 from weather import Weather
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8000)
