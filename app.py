a231ae95bc3793c099a2991e94b0287d473ba9af
Megan Amendola <megmrob@gmail.com>
2020-07-20 15:02:14 -0500

    added flask and started wireframing with html

--- app.py
+++ app.py
@@ -3,3 +3,13 @@ from gcalendar import Gcalendar
 from grocery import Glist
 from todos import Todos
 from weather import Weather
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=8000)
